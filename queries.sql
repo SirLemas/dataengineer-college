811fa300cb8dcee39aa4927a1b7d79e36185c2e5
Talles Nogueira <talles509@gmail.com>
2023-06-12 23:03:40 -0300

    adicionando arquivo de sql para rodar antes de rodar o projeto

---
+++ queries.sql
@@ -0,0 +1,24 @@
+create database dados_brutos;
+
+create table saude(
+	id serial primary key,
+	geocodigo text,
+	municipio text,
+	variavel text,
+	ano text,
+	quantidade_profissionais_saude text,
+	und text,
+	tags text,
+	fonte text
+);
+
+create table dados_refinados (
+	id serial primary key,
+	codigo_municipio text,
+	nome_municipio_aluno text,
+	ano text,
+	ensino_rede text,
+	ensino_tipo text,
+	tipo_profissional_saude text,
+	quantidade_profissionais_saude text,
+);
